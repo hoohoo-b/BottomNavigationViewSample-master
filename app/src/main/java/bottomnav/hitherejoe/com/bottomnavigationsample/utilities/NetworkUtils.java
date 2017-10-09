c64c9ca91be2ab1607526798320544424e0c7b50
alletstan <tanpeiting96@hotmail.com>
2017-10-09 21:58:06 +0800

    need adjustment, added connection to internet

---
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/utilities/NetworkUtils.java
@@ -0,0 +1,8 @@
+package bottomnav.hitherejoe.com.bottomnavigationsample;
+
+/**
+ * Created by Allets on 9/10/2017.
+ */
+
+public class NetworkUtils {
+}
---
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/utilities/RecipeAdapter.java
@@ -0,0 +1,8 @@
+package bottomnav.hitherejoe.com.bottomnavigationsample.utilities;
+
+/**
+ * Created by Allets on 9/10/2017.
+ */
+
+public class RecipeAdapter {
+}
