3d825f0dd64ccf83a0e28f3efd24c267e9e5012f
sohjunjie <junjie.soh93@gmail.com>
2017-10-30 09:36:00 +0800

    create favourite recipe skeleton

--- app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/FavRecipeFragment.java
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/FavRecipeFragment.java
@@ -3,6 +3,7 @@ package bottomnav.hitherejoe.com.bottomnavigationsample;
 // FOR FAVOURITE ACTIVITY... CURRENTLY BLANK
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,7 +11,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import bottomnav.hitherejoe.com.bottomnavigationsample.utilities.JsonReader;
+import bottomnav.hitherejoe.com.bottomnavigationsample.utilities.NetworkUtils;
 import bottomnav.hitherejoe.com.bottomnavigationsample.utilities.RecipeAdapter;
 
 
@@ -19,7 +24,10 @@ public class FavRecipeFragment extends Fragment implements RecipeAdapter.ListIte
     private static final int NUM_LIST_ITEMS = 100;
     private RecyclerView mRecyclerView;
     private RecipeAdapter mRecipeAdapter;
+    private ProgressBar mLoadingIndicator;
+    private TextView mErrorMessageDisplay;
     private Context appContext;
+    private String authToken;
 
     public static FavRecipeFragment newInstance() {
         FavRecipeFragment fragment = new FavRecipeFragment();
@@ -33,18 +41,70 @@ public class FavRecipeFragment extends Fragment implements RecipeAdapter.ListIte
 
         View rootView = inflater.inflate(R.layout.fragment_favourite, container, false);
         appContext = getActivity().getApplicationContext();
+        authToken = MyApplication.getAuthToken();
 
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.rv_favrecipelist);
+        mLoadingIndicator = (ProgressBar) rootView.findViewById(R.id.pb_loading_indicator);
+        mErrorMessageDisplay = (TextView) rootView.findViewById(R.id.tv_error_message_display);
+
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setHasFixedSize(true);
         mRecipeAdapter = new RecipeAdapter(appContext, NUM_LIST_ITEMS, this);
         mRecyclerView.setAdapter(mRecipeAdapter);
-//
-//        mRecyclerView.setVisibility(View.VISIBLE);
+
+        mRecyclerView.setVisibility(View.VISIBLE);
 
         return rootView;
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+//        get list of favourite recipes
+    }
+
+//    private void showRecipeListDataView() {
+//        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+//        mRecyclerView.setVisibility(View.VISIBLE);
+//    }
+
+    public class FetchFavouriteRecipeAsyncTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            String urlString = params[0];
+            String authToken = params[1];
+
+            String output;
+            String[] recipeList = null;
+
+            try {
+                output = NetworkUtils.getResponseFromHttpUrl(urlString, requestMethod, authToken);
+                recipeList = JsonReader.retrieveRecipeList(output);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return recipeList;
+        }
+
+        @Override
+        protected void onPostExecute(String[] recipeListData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (recipeListData != null) {
+//                showRecipeListDataView();
+                mRecipeAdapter.setRecipeNameListData(recipeListData);
+            } else {
+//                showErrorMessage();
+            }
+        }
+
     }
 
 
@@ -60,6 +120,5 @@ public class FavRecipeFragment extends Fragment implements RecipeAdapter.ListIte
 //        intentToStartDetailActivity.putExtra("RecipeDetails", recipeList);
 //        startActivity(intentToStartDetailActivity);
     }
-
 
 }
