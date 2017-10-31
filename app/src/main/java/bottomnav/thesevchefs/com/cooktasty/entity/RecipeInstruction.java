5ba2d20567f294b5b1b66f32d5577353cdbd5913
sohjunjie <junjie.soh93@gmail.com>
2017-11-01 03:04:54 +0800

    change method to public

--- app/src/main/java/bottomnav/thesevchefs/com/cooktasty/entity/RecipeInstruction.java
+++ app/src/main/java/bottomnav/thesevchefs/com/cooktasty/entity/RecipeInstruction.java
@@ -3,12 +3,12 @@ package bottomnav.thesevchefs.com.cooktasty.entity;
 import java.sql.Time;
 
 /**
- * Created by Admin on 31/10/2017.
+ * Created by Jun Jie on 31/10/2017.
  */
 
 public class RecipeInstruction {
-    int step_num;
-    String instruction;
-    Time time_required;
-    String image_url;
+    public int step_num;
+    public String instruction;
+    public Time time_required;
+    public String image_url;
 }
